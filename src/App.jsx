1b54ed4796b375dffdd21d1e7accbc61d5c50e70
dennisvanleeuwen <bendora@gmail.com>
2025-06-11 11:53:24 +0200

    App.jsx Import fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import { useGSAP } from "@gsap/react";
 
 
 // import components
-import Header from './components/header';
-import Footer from './components/footer';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 
 function App() {
